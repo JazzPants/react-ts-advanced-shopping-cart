e3e17c22e171fa0f8ed6c8dc1741371ebec378a0
Leighton <leighton.l14@gmail.com>
2023-10-03 01:23:11 +1100

    update navbar, add svg cart asset

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,30 @@
-import {Container, Navbar as NavbarBs} from "react-bootstrap"
+import {Button, Container, Nav, Navbar as NavbarBs} from "react-bootstrap"
+import { NavLink } from "react-router-dom"
 
 export function Navbar() {
 return (
-<NavbarBs className="bg-white shadow-sm mb-3">
-  <Container>Nav</Container>
+<NavbarBs sticky="top" className="bg-white shadow-sm mb-3">
+  <Container>
+    <Nav className="me-auto">
+      <Nav.Link to="/" as={NavLink}>Home</Nav.Link>
+      <Nav.Link to="/store" as={NavLink}>Store</Nav.Link>
+      <Nav.Link to="/about" as={NavLink}>About</Nav.Link>
+    </Nav>
+    <Button style={{width: "3rem", height: "3rem", position: "relative"}} variant="outline-primary" className="rounded-circle">
+      <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M6.29977 5H21L19 12H7.37671M20 16H8L6 3H3M9 20C9 20.5523 8.55228 21 8 21C7.44772 21 7 20.5523 7 20C7 19.4477 7.44772 19 8 19C8.55228 19 9 19.4477 9 20ZM20 20C20 20.5523 19.5523 21 19 21C18.4477 21 18 20.5523 18 20C18 19.4477 18.4477 19 19 19C19.5523 19 20 19.4477 20 20Z" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+      </svg>
+      <div className="rounded-circle bg-danger d-flex justify-content-center align-items-center" style={{
+      color: "white", 
+      width: "1.5rem", 
+      height: "1.5rem", 
+      position: "absolute",
+      bottom: 0,
+      right: 0,
+      transform: "translate(25%, 25%)",
+      }}>3</div>
+    </Button>
+  </Container>
 </NavbarBs>
 )
 }
