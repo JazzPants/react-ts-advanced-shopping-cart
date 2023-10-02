24e7311879b5c6065d1d689c0c1ac253cf343ea0
Leighton <leighton.l14@gmail.com>
2023-10-03 00:21:00 +1100

    create basic pages and add them to App routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 
 import {Routes, Route} from "react-router-dom"
 import {Container} from "react-bootstrap"
+import { Home } from "./pages/Home"
+import { Store } from "./pages/Store"
+import { About } from "./pages/About"
 
 function App() {
   return (
   <Container>
-  <h1>Hello</h1>
+  <h1>React Typescript Shopping Cart</h1>
+  <Routes>
+    <Route path="/" element={<Home/>} />
+    <Route path="/store" element={<Store/>} />
+    <Route path="/about" element={<About/>} />
+  </Routes>
   </Container>
   )
 }
